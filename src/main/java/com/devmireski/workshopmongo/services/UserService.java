71a79d50fcbcb6fb785c2737d671eef4a2daa7a0
Gustavo Mireski Demetrio <conta1testegustavo@gmail.com>
2023-08-01 20:17:48 -0300

    Obtendo um usuario por id

--- src/main/java/com/devmireski/workshopmongo/services/UserService.java
+++ src/main/java/com/devmireski/workshopmongo/services/UserService.java
@@ -1,20 +1,28 @@
 package com.devmireski.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devmireski.workshopmongo.domain.User;
 import com.devmireski.workshopmongo.repository.UserRepository;
+import com.devmireski.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
